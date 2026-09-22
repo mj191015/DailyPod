40c10fd7bf545596f5589fe1e36ba30a4ac35b92
yesri <jys0103@gnu.ac.kr>
2026-09-22 18:37:11 +0900

    Add new output line in Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello DailyPod!");
         System.out.println("arin");
+        System.out.println("seul");
     }
 }
